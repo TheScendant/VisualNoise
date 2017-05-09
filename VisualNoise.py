6dea9aca783f38ad2f32902253d80f6a34761ebd
hmn8929 <hmn8929@mail.rit.edu>
2017-05-09 00:22:49 -0400

    Performs fft on left and right channel. Converts complex numbers to their magnitude.

--- VisualNoise.py
+++ VisualNoise.py
@@ -1,5 +1,5 @@
-import sys,wave,struct
-import cv2
+import sys,wave,struct,math,os,time
+#import cv2
 import numpy as np
 from scipy.fftpack import fft
 import pyaudio
@@ -16,19 +16,32 @@ def main():
 
     p = pyaudio.PyAudio()
     pFormat = p.get_format_from_width(wf.getsampwidth())
-    pMax = (2.0 ** (p.get_sample_size(pFormat) * 8 - 1))
+    pMax = float(2.0 ** (p.get_sample_size(pFormat) * 8 - 1))
     pChannels = wf.getnchannels()
-    pRate = wf.getframerate()
+    pRate = wf.getframerate()                           #Sampling Rate --- 44,1000
     stream = p.open(format = pFormat,channels=pChannels,rate=pRate,output=True)
 
     frameCount = 512
 
-    data = wf.readframes(frameCount)
+    data = wf.readframes(frameCount)                    #start frame
     while data != '':
-        array = np.array(struct.unpack("%dh" % (pChannels*frameCount),data))/pMax
+        array = np.array(struct.unpack("%dh" % (pChannels*frameCount),data))/pMax   #normalize 1 to -1
 
-        stream.write(data)
-        data = wf.readframes(frameCount)
+        left = array[::2]
+        leftFFT = np.fft.fft(left,frameCount)
+        lPower = [math.sqrt( (x.real **2) + x.imag **2) for x in leftFFT]
+        
+        right = array[1::2]
+        rightFFT = np.fft.fft(right,frameCount)
+        rPower = [math.sqrt( (x.real **2) + x.imag **2) for x in rightFFT]
+        
+        #fSpace = pRate/float(len(lPower))               #Frequency Spacing. len(lPower)
+        
+        #graphPowerArrays
+
+
+        stream.write(data)                              #play
+        data = wf.readframes(frameCount)                #next frame
 
     stream.stop_stream()
     stream.close()
