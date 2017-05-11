dfea214dbd3f1096b0ea66055a002bb9f5484996
hmn8929 <hmn8929@mail.rit.edu>
2017-05-10 22:19:54 -0400

    FFT performed on 2048 frames at a time. Binning into c2,f2 through c5 by frequency. Using ascii_graph library for print out.

--- VisualNoise.py
+++ VisualNoise.py
@@ -1,14 +1,22 @@
+"""
+Author: Hayden M Nier
+Date: May 10, 2017
+"""
+
+
 import sys,wave,struct,math,os,time
 #import cv2
 import numpy as np
 from scipy.fftpack import fft
 import pyaudio
+from ascii_graph import Pyasciigraph
 
 
 def main():
     try:
         filename = sys.argv[1]
     except:
+        print "Include custom song as command line arg if desired."
         print "Defaulting to Crystallize.wav"
         filename = "Crystallize.wav"
 
@@ -16,16 +24,27 @@ def main():
 
     p = pyaudio.PyAudio()
     pFormat = p.get_format_from_width(wf.getsampwidth())
-    pMax = float(2.0 ** (p.get_sample_size(pFormat) * 8 - 1))
+    pSS = p.get_sample_size(pFormat)
+    pMax = float(2.0 ** (pSS * 8 - 1))
     pChannels = wf.getnchannels()
-    pRate = wf.getframerate()                           #Sampling Rate --- 44,1000
+    pRate = wf.getframerate()                           #Sampling Rate --- 44.1 kHz
     stream = p.open(format = pFormat,channels=pChannels,rate=pRate,output=True)
 
-    frameCount = 512
-
+    frameCount = 2048
+    fSpace = pRate/frameCount
+    #bins = makeBins(pRate);
     data = wf.readframes(frameCount)                    #start frame
+
+    
+    #c2,f2,c3,f3,c4,f4,c5
+    cs = [65.4,87.3,130.8,174.6,261.6,349,523.2]
+
+
+#    cmax = 0
+    #for hax in range(3):
+    barNum = 0
     while data != '':
-        array = np.array(struct.unpack("%dh" % (pChannels*frameCount),data))/pMax   #normalize 1 to -1
+        array = np.array(struct.unpack("%dh" % (pChannels*frameCount),data)) /pMax   #normalize 1 to -1
 
         left = array[::2]
         leftFFT = np.fft.fft(left,frameCount)
@@ -34,11 +53,20 @@ def main():
         right = array[1::2]
         rightFFT = np.fft.fft(right,frameCount)
         rPower = [math.sqrt( (x.real **2) + x.imag **2) for x in rightFFT]
-        
-        #fSpace = pRate/float(len(lPower))               #Frequency Spacing. len(lPower)
-        
-        #graphPowerArrays
+        #print lPower    
 
+        bars = calcBar(lPower,rPower,cs,fSpace)
+        if not sum(bars) == 0:
+            bars = [(1,x) for x in bars]
+            #bars.append([1,1])
+            os.system('clear')
+            graph = Pyasciigraph()
+            line = graph.graph('Visualizer',bars)
+            for x in range(len(line)):
+                print (line[x])
+            print "###############################################################################"
+        #fSpace = pRate/float(len(lPower))               #Frequency Spacing.
+        #print fSpace
 
         stream.write(data)                              #play
         data = wf.readframes(frameCount)                #next frame
@@ -47,4 +75,23 @@ def main():
     stream.close()
     p.terminate()
 
+
+def calcBar(lPower,rPower,cs,fSpace):
+    bars = [0.0 for x in range(len(cs))]
+    sizes = [0 for x in range(len(cs))]
+    currBar = 0
+    for x in range(1,(len(lPower) /2)+1):
+        if (x*fSpace) < cs[currBar]:
+            bars[currBar] +=((lPower[x] + rPower[x])/2.0)
+            sizes[currBar] +=1
+        elif (x*fSpace) > cs[-1]:
+            break
+        else:
+            currBar +=1
+            bars[currBar] +=((lPower[x] + rPower[x])/2.0)
+            sizes[currBar]+=1
+    for x in range(len(bars)):
+        if not sizes[x]  == 0:
+            bars[x] = bars[x] / float(sizes[x])
+    return bars    
 main()
